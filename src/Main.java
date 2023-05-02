621d6b3332092eb2cca5fbd14f767850eddec71b
TenGreen <Grishintut@yandex.ru>
2023-05-02 16:52:51 +0300

    Попробовал создать массив студентов Гриффиндор и распечатать первого студента

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,26 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        //Random r = new Random();
-        //System.out.println("r.nextInt() = " + r.nextInt(100));
+        String [] GryffindorList = new String[] {"DracoMalfoy",
+                "GrahamMontague", "GregoryGoyle"};
+        GryffindorStudent[] GryffindorStudent = new GryffindorStudent[GryffindorList.length];
 
-        //HarryPotter HermioneGranger RonWeasley
-        GryffindorStudent HarryPotter = new GryffindorStudent("HarryPotter", gR100(),
+        for (int i = 0; i < GryffindorList.length; i++) {
+            GryffindorStudent [i] = new GryffindorStudent(GryffindorList[i], gR100(),
+                    gR100(), gR100(), gR100(),
+                    gR100());
+
+        }
+
+        /*GryffindorStudent HarryPotter = new GryffindorStudent("HarryPotter", gR100(),
                 gR100(), gR100(), gR100(),
                 gR100());
-
+*/
         SlytherinStudent DracoMalfoy = new SlytherinStudent("Draco Malfoy", gR100(), gR100(),gR100(),
                 gR100(),gR100(),gR100(), gR100());
         //Draco Malfoy GrahamMontague GregoryGoyle
 
-        HarryPotter.PrintDescriptionStudent();
+        GryffindorStudent[0].PrintDescriptionStudent();
 
     }
 
